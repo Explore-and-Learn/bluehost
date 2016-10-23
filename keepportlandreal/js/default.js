8c1ce8c341352882bd9d32e260c6ae3b7ac3d68e
marty-hope <marty.hope@gmail.com>
2016-10-23 12:07:28 -0700

    Add less and js implementations for testing

--- keepportlandreal/js/default.js
+++ keepportlandreal/js/default.js
@@ -1,2 +1,20 @@
 (function () {
+
+	"use strict"
+
+	/**
+	 * Open's CNN web site
+	 */
+	function openCnn() {
+		open("http://cnn.com");
+	}
+
+	$(document).ready(function(){
+
+			$("#mainMenu").on("click", function(){
+				openCnn();
+			})
+
+	})
+
 })();
