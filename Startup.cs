fc46b44783141a7188faff398016e1e634fbbde1
Marty Hope <marty.hope@live.com>
2018-02-10 16:16:42 -0800

    Update solution to support REST service

---
+++ Startup.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace martyhope.com
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true
+            };
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.DefaultFilesOptions.DefaultFileNames = new[] { "default.html" };
+
+            app.UseFileServer(options)
+                .UseDefaultFiles()
+                .UseStaticFiles(new StaticFileOptions
+                    {
+                        OnPrepareResponse = ctx =>
+                        {
+                            // Requires the following import:
+                            // using Microsoft.AspNetCore.Http;
+                            ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=600");
+                        }
+                    }
+                )
+                .UseMvc();
+        }
+    }
+}
