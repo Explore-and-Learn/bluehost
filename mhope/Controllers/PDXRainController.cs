f82a616e3593caa187f22e093fc4014bb8894191
Marty Hope <marty.hope@live.com>
2018-04-08 16:18:44 -0700

    Add method that returns a specific number of results. Update to return results using async/await pattern

--- mhope/Controllers/PDXRainController.cs
+++ mhope/Controllers/PDXRainController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RainGauge;
@@ -10,10 +12,31 @@ namespace martyhope.com.Controllers
     [Route("api/pdxrainfall")]
     public class PdxRainController : Controller
     {
+        /// <summary>
+        /// Return all of the reporting stations data ordered by the water accumulation result
+        /// </summary>
+        /// <returns cref="IEnumerable&lt;PDXRainGauge.PrecipitationData&gt;"/>
         [HttpGet]
-        public IEnumerable<PDXRainGauge.PrecipitationData> Get()
+        public async Task<IEnumerable<PDXRainGauge.PrecipitationData>> GetAllOrderedByWaterYearAccumulationAmount()
         {
-            return PDXRainGauge.pdxRainfallRecords.Value.OrderByDescending(x => x.WaterYearAccumulation);
+            return await Task<IEnumerable<PDXRainGauge.PrecipitationData>>.Factory.StartNew(
+                () =>
+                {
+                    IEnumerable<PDXRainGauge.PrecipitationData> pdxRainData = RainGauge.PDXRainGauge.pdxRainfallRecords.Value;
+                    return pdxRainData.OrderByDescending(x => x.WaterYearAccumulation);
+                }, CancellationToken.None);
+
+        }
+
+        /// <summary>
+        /// Return the requested number of reporting stations data ordered by the water accumulation result
+        /// </summary>
+        /// <returns cref="IEnumerable&lt;PDXRainGauge.PrecipitationData&gt;"/>
+        [HttpGet("{i}")]
+        public IEnumerable<PDXRainGauge.PrecipitationData> Get(int i)
+        {
+            IEnumerable<PDXRainGauge.PrecipitationData> pdxRainData = RainGauge.PDXRainGauge.pdxRainfallRecords.Value;
+            return pdxRainData.OrderByDescending(x => x.WaterYearAccumulation).Take(i);
 
         }
     }
