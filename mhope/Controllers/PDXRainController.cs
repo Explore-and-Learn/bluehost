8cca0ac29ca98589fd88f26851610faf7af246de
Marty Hope <marty.hope@live.com>
2018-04-08 13:29:38 -0700

    Add pdxrainfall controller

---
+++ mhope/Controllers/PDXRainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace martyhope.com.Controllers
+{
+    [Route("api/pdxrainfall")]
+    public class PdxRainController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<RainGauge.PDXRainGauge.PrecipitationData> Get()
+        {
+            return RainGauge.PDXRainGauge.pdxRainfallRecords.Value;
+
+        }
+    }
+}
