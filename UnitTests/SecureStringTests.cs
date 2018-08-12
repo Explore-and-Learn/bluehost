37762df57d00498294aa95c3a60fe00aab53f9fb
Marty Hope <marty.hope@live.com>
2018-08-12 10:53:22 -0700

    Add using statement to ensure SecureString Dispose() is called.

--- UnitTests/SecureStringTests.cs
+++ UnitTests/SecureStringTests.cs
@@ -10,15 +10,17 @@ namespace UnitTests
         {
             string testString = "This is a test string to see if the helper class can reveal the value.";
             var array = testString.ToCharArray();
-            SecureString ss = new SecureString();
-            foreach (var c in array)
+            using (SecureString ss = new SecureString())
             {
-                ss.AppendChar(c);
+                foreach (var c in array)
+                {
+                    ss.AppendChar(c);
+                }
+
+                var revealedValue = ss.SecureStringToString();
+
+                Assert.Equal(testString, revealedValue);
             }
-
-            var revealedValue = ss.SecureStringToString();
-
-            Assert.Equal(testString, revealedValue);
 
 
         }
